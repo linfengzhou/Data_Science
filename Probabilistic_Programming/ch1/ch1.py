c6141305143e587d777ab467f016039eb604db07
Linfeng Zhou <luke.zlf@gmail.com>
2015-09-13 01:51:56 -0400

    Inferring behaviour from text-message data

---
+++ Probabilistic_Programming/ch1/ch1.py
@@ -0,0 +1,23 @@
+import numpy as np 
+import pymc as pm
+from matplotlib import pyplot as plt
+%matplotlib inline
+import os 
+os.chdir("/Users/luke/Dropbox/data_science/Probabilistic_programming/ch1")
+figsize(12.5, 3.5)
+count_data = np.loadtxt("data/txtdata.csv")
+n_count_data = len(count_data)
+plt.bar(np.arange(n_count_data), count_data, color="#348ABD")
+plt.xlabel("Time (days)")
+plt.ylabel("count of text-msgs received")
+plt.title("Did the user's texting habits change over time?")
+plt.xlim(0, n_count_data)
+
+
+alpha = 1.0 / count_data.mean()  # Recall count_data is the
+                               # variable that holds our txt counts
+lambda_1 = pm.Exponential("lambda_1", alpha)
+lambda_2 = pm.Exponential("lambda_2", alpha)
+
+tau = pm.DiscreteUniform("tau", lower=0, upper=n_count_data)
+print "Random output:", tau.random(), tau.random(), tau.random()
